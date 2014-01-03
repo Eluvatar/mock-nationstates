87addc8b030a1d2e11a4a6776dd65ccc7849c2e3
Eluvatar <drstadler@gmail.com>
2014-01-03 01:16:09 -0500

    Removing accidentally added imports

--- ns.py
+++ ns.py
@@ -13,7 +13,6 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import json, mmap, cherrypy
 import string,codecs
 
 __id_str__trans=string.maketrans(" ","_")
